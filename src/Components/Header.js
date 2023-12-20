1165fa29bee75934269b4cb3ca2083eb42082fc0
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2023-12-20 11:08:50 +0300

    menu problem is solved.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,35 +1,56 @@
-import React from 'react'
-import { Container, Navbar, Nav, NavbarBrand,NavbarToggle } from 'react-bootstrap'
+import React, { useState } from 'react';
+import { Container, Navbar, Nav } from 'react-bootstrap';
 import logo from '../Images/brightLogo7.png';
-import { NavLink } from 'react-router-dom'
-
-
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
+  const [showOffcanvas, setShowOffcanvas] = useState(false);
+
+  const handleToggleOffcanvas = () => {
+    setShowOffcanvas(!showOffcanvas);
+  };
+
+  const handleOffcanvasClose = () => {
+    setShowOffcanvas(false);
+  };
+
   return (
     <>
-    
-      <Navbar collapseOnSelect expand="lg" className='navbar' sticky="top">
-        <Container >
-        <NavbarBrand >
-          <NavLink className="nav-link" to="/"> <img className='logo' src={logo} /></NavLink>
-         
-        </NavbarBrand>
-        <NavbarToggle aria-controls="responsive-navbar-nav"  className='custom-toggle '/>
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ms-auto">
-              <NavLink className="nav-link   nav-item "  to="/">Home</NavLink>
-            <NavLink className="nav-link nav-item" to="/about-us">About Us</NavLink>
-            <NavLink className="nav-link nav-item " to="/courses">Courses</NavLink>
-            <NavLink className="nav-link nav-item" to="/instructors">Instructors</NavLink>
-            <NavLink className="nav-link nav-item" to="/contact">Contact</NavLink>
-          </Nav>
-         
-        </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <div>
+        {['lg'].map((expand) => (
+          <Navbar key={expand} expand={expand} className="navbar">
+            <Container fluid className='mx-5'>
+              <Navbar.Brand><NavLink className="nav-link" to="/"> <img className='logo' src={logo} alt="Logo" /></NavLink></Navbar.Brand>
+              <Navbar.Toggle style={{ backgroundColor: 'white' }} aria-controls={`offcanvasNavbar-expand-${expand}`} onClick={handleToggleOffcanvas} />
+              <Navbar.Offcanvas
+                id={`offcanvasNavbar-expand-${expand}`}
+                aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                placement="end"
+                show={showOffcanvas}
+                onHide={handleOffcanvasClose}
+              >
+                <Offcanvas.Header closeButton style={{ backgroundColor: '#0b123f', color: 'white' }}>
+                  <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                    Menu
+                  </Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body style={{ backgroundColor: '#0b123f' }}>
+                  <Nav className="justify-content-end flex-grow-1 pe-3 ">
+                    <NavLink onClick={handleOffcanvasClose} aria-current="page" className="nav-link nav-item" to="/">Home</NavLink>
+                    <NavLink onClick={handleOffcanvasClose} className="nav-link nav-item" to="/about-us">About Us</NavLink>
+                    <NavLink onClick={handleOffcanvasClose} className="nav-link nav-item" to="/courses">Courses</NavLink>
+                    <NavLink onClick={handleOffcanvasClose} className="nav-link nav-item" to="/instructors">Instructors</NavLink>
+                    <NavLink onClick={handleOffcanvasClose} className="nav-link nav-item" to="/contact">Contact</NavLink>
+                  </Nav>
+                </Offcanvas.Body>
+              </Navbar.Offcanvas>
+            </Container>
+          </Navbar>
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
